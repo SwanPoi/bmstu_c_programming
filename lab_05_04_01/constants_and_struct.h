932912df941315e9c342df0fdb380111c47563d4
lva21u510 <vov-0308@mail.ru>
2022-05-26 12:07:13 +0300

    Try to correct Task №4

--- lab_05_04_01/constants_and_struct.h
+++ lab_05_04_01/constants_and_struct.h
@@ -4,7 +4,7 @@
 #define ERR_OK 0
 #define ERR_INVALID_ARGS 53
 #define ERR_WRITE 3
-#define ERR_OPEN 53
+#define ERR_OPEN 4
 #define ERR_READ_STRING 5
 #define ERR_READ 6
 #define ERR_READ_MARKS 7
--- lab_05_04_01/func_tests/scripts/comparator.sh
+++ lab_05_04_01/func_tests/scripts/comparator.sh
@@ -4,7 +4,7 @@ if [[ $# -ne 2 ]]; then
   exit 1
 fi
 
-if diff "$1" "$2";  then
+if diff "$1" "$2" > ../../diff.txt;  then
   exit 0
 fi
 exit 1 
