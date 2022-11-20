97bc2325efbdd50b13c402d6b6d78ace2481a93e
lva21u510 <vov-0308@mail.ru>
2022-11-20 22:17:27 +0300

    Try to fix codestyle

--- lab_10_01_01/src/main.c
+++ lab_10_01_01/src/main.c
@@ -8,65 +8,65 @@
 
 int main(int argc, char *argv[])
 {
-	int rc = ERR_OK;
+    int rc = ERR_OK;
 	
-  if (argc == 3 || argc == 4)
-  {
-    FILE *first_in_file = fopen(argv[1], "r");
-        
-    if (first_in_file)
+    if (argc == 3 || argc == 4)
     {
-      node_t *first_films = NULL;
+        FILE *first_in_file = fopen(argv[1], "r");
+        
+        if (first_in_file)
+        {
+            node_t *first_films = NULL;
 
-      rc = init_struct_list(first_in_file, &first_films);
+            rc = init_struct_list(first_in_file, &first_films);
 
-      fclose(first_in_file);
+            fclose(first_in_file);
 
-      if (rc == ERR_OK && argc == 4)
-      {
-        FILE *second_in_file = fopen(argv[2], "r");
+            if (rc == ERR_OK && argc == 4)
+            {
+                FILE *second_in_file = fopen(argv[2], "r");
 
-        if (second_in_file)
-		    {
-		      node_t *second_films = NULL;
+                if (second_in_file)
+		            {
+		                node_t *second_films = NULL;
 
-		      rc = init_struct_list(second_in_file, &second_films);
+		                rc = init_struct_list(second_in_file, &second_films);
 
-		      fclose(second_in_file);
+		                fclose(second_in_file);
 
-		      if (rc == ERR_OK)
-		        append(&first_films, &second_films);
-		    }
-		  }
+		                if (rc == ERR_OK)
+		                    append(&first_films, &second_films);
+		            }
+		        }
 
-		  if (rc == ERR_OK)
-		  {
-		    first_films = sort(first_films, films_comparator);
+		        if (rc == ERR_OK)
+		        {
+		            first_films = sort(first_films, films_comparator);
 
-		    film_t *cheap_film = pop_front(&first_films);
-		    free_film(cheap_film);
+		            film_t *cheap_film = pop_front(&first_films);
+		            free_film(cheap_film);
 
-		    film_t *expensive_film = pop_back(&first_films);
-		    free_film(expensive_film);
+		            film_t *expensive_film = pop_back(&first_films);
+		            free_film(expensive_film);
 
-		    FILE *dest = fopen(argv[argc - 1], "w");
+		            FILE *dest = fopen(argv[argc - 1], "w");
 
-		    if (dest)
-		    {
-		    	rc = print_all(dest, first_films);
-		    	fclose(dest);
-		    }	
-		    else
-		    	rc = ERR_OPEN_DEST;
-		  } 
+		            if (dest)
+		            {
+                    rc = print_all(dest, first_films);
+                    fclose(dest);
+		            }	
+		            else
+                    rc = ERR_OPEN_DEST;
+		        } 
 
-      free_list(first_films); 
+            free_list(first_films); 
+        }
+        else
+            rc = ERR_OPEN;
     }
-      else
-        rc = ERR_OPEN;
-  }
-  else
-    rc = ERR_INVALID_ARGS;
+    else
+        rc = ERR_INVALID_ARGS;
     
-  return rc;
+    return rc;
 }
